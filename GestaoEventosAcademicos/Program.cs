56c6a018ad4ca7c126fd470f09545f8dcb3d071e
LuizBaldini <luizfelipebaldini.07@gmail.com>
2025-03-26 21:03:13 -0300

    Agoravai

---
+++ GestaoEventosAcademicos/Program.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using GestaoEventosAcademicos.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Configuração do banco de dados
+builder.Services.AddDbContext<Context>(options =>
+    options.UseSqlServer(builder.Configuration["Data:GestaoEventosAcademicos:ConnectionString"]));
+
+// Configuração do Identity
+builder.Services.AddIdentity<Usuario, IdentityRole>()
+    .AddEntityFrameworkStores<Context>()
+    .AddDefaultTokenProviders();
+
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Login/Index"; // Redireciona para a tela de login
+    options.AccessDeniedPath = "/Home/AcessoNegado"; // Tela de erro de acesso negado
+});
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(30);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    await SeedData.EnsurePopulated(app);
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+
+app.UseStaticFiles();
+app.UseSession();
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Login}/{action=Index}/{id?}");
+
+
+app.Run();
+
+
